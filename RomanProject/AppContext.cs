ef1e2e0e6dbc0c432c6aeaa1b94b064f1ed7d21c
lluxweet <nikadovgan2004@gmail.com>
2024-02-02 00:29:55 +0700

    чудо проект для романа

---
+++ RomanProject/AppContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RomanProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanProject
+{
+    
+    internal class AppContext: DbContext
+    {
+        public DbSet<RoleEntity> roles { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=dbRoman.db");
+        }
+    }
+}
